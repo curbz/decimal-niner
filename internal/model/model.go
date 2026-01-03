e6c02380ed2cf70afb3caef096657e5bac32b3ea
curbz <morris.daz@googlemail.com>
2026-01-03 22:32:21 +0000

    reordering of model structs

--- internal/model/model.go
+++ internal/model/model.go
@@ -2,22 +2,15 @@ package model
 
 import "time"
 
-// Position
-type Position struct {
-	Lat      float64
-	Long     float64
-	Altitude float64
-	Heading  float64
+type Aircraft struct {
+	Flight       Flight
+	Type         string
+	Class        string
+	Code         string
+	Airline      string
+	Registration string
 }
 
-type Comms struct {
-	Callsign         string
-	Frequency        float64
-	LastTransmission string
-	LastInstruction  string
-}
-
-// Flight
 type Flight struct {
 	Position    Position
 	FlightNum   int64
@@ -28,18 +21,23 @@ type Flight struct {
 	Comms       Comms
 }
 
+type Position struct {
+	Lat      float64
+	Long     float64
+	Altitude float64
+	Heading  float64
+}
+
 type Phase struct {
 	Current   int
 	Previous  int	// used for detecting changes, previous refers to last update and not necessarily the actual previous phase
 	Transition time.Time
 }
 
-// Aircraft
-type Aircraft struct {
-	Flight       Flight
-	Type         string
-	Class        string
-	Code         string
-	Airline      string
-	Registration string
+type Comms struct {
+	Callsign         string
+	Frequency        float64
+	LastTransmission string
+	LastInstruction  string
 }
+
