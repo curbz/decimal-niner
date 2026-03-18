e7f1d6820f99cca51394a19847b7c049c5fb6de2
Darren Morris <morris.daz@googlemail.com>
2026-03-18 08:35:42 +0000

    enable config path setting via env var for tests

--- internal/atc/atc_test.go
+++ internal/atc/atc_test.go
@@ -1,12 +1,29 @@
 package atc
 
 import (
+	"log"
 	"os"
 )
 
-func init() {
 	// This runs before any tests in this package
-	// TODO: how can we make this work for custom config locations?
-	// We move up two levels to the root of the repo so that config.yaml and /resources is found
-	_ = os.Chdir("../../")
+func init() {
+
+	log.Println("running prerequisite atc package init function")
+
+	// Check for custom config file location
+	configPath := os.Getenv("D9_CONFIG_PATH")
+
+	if configPath == "" {
+		// Use default config location.
+		// Move up two levels to the root of the repo so that config.yaml and /resources is found
+		configPath = "../.."
+	} else {
+		log.Println("loading configuration from custom location", configPath)
+	}
+
+	err := os.Chdir(configPath)
+	if err != nil {
+		log.Fatalf("test execution failed - error loading configuration: %v", err)
+	}
+
 }
